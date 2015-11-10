e0958efe248e5fdc02a67ad8423d4baafcbc66fc
Miguel Dell'Uomini <delldor@gmail.com>
2015-11-10 17:13:35 -0430

    Create PeazipGTK.sh
    Contiene también QT, pero no funciona el programa al instalar por el error:}
    ...undefined symbol: QApplication_hook_hook_commitDataRequest

---
+++ PeazipGTK.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#~ TODO: si ya está instalado Peazip, ofrecer desinstalarlo primero
+#~ Si ya está instalada la dependencia, no buscar descargarla ni instalarla
+
+cd /tmp
+
+paquete="peazip"
+servidor="https://packages.debian.org/squeeze/i386/libgmp3c2/download"
+
+tempo=$(mktemp)
+wget -qO- $servidor|sed -n 's/.*href="\([^"]*\).*/\1/p'|grep .deb$ |sort -r > $tempo
+paque=$(cat $tempo| head -n1)
+wget -c $paque && sudo cp -vu `basename $paque` /var/cache/apt/archives
+
+sudo gdebi-gtk `basename $paque`
+
+###############################
+
+paquete="peazip"
+servidor="https://github.com/giorgiotani/PeaZip/releases/"
+
+tempo=$(mktemp)
+#~ Descarga lista de paquetes disponibles en ese servidor
+
+wget -qO- $servidor|sed -n 's/.*href="\([^"]*\).*/\1/p'|grep .deb$ |sort > $tempo
+cat $tempo
+
+servidor="https://github.com/"
+
+#~ Para QT
+#~ paque=$(grep -E "*.Qt-2_i386.deb" $tempo|tail -n1)
+
+#~ Para GTK
+paque=$(grep -E "*.GTK2-2_i386.deb" $tempo|tail -n1)
+
+echo "
+
+##################### 
+A descargar:
+$servidor/$paque"
+
+wget -c $servidor/$paque && sudo cp -vu `basename $paque` /var/cache/apt/archives
+
+sudo gdebi-gtk `basename $paque`
+
+read -p "Listo"
