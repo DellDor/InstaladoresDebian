cf7c3d0abd7ebebf4ddf895fc4df7b7cd02c1ca9
Miguel Dell'Uomini <delldor@gmail.com>
2016-02-05 10:14:51 -0430

    Update sparky-mate.sh

--- sparky-mate.sh
+++ sparky-mate.sh
@@ -8,7 +8,7 @@ export sector="mate"
 export version="4.2-i586"
 export descargar="$servidor/$sector/sparkylinux-$version-$sector.iso.package-list.txt"
 
-wget -c https://raw.githubusercontent.com/DellDor/InstaladoresDebian/master/creameta-de-lista-sh
+wget -c https://raw.githubusercontent.com/DellDor/InstaladoresDebian/master/creameta-de-lista.sh
 chmod a+x creameta-de-lista.sh
 
 exec ./creameta-de-lista.sh
