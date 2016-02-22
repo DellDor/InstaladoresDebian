679fb09594cda6440688fa09d92ef51da1a7dbae
Miguel Dell'Uomini <delldor@gmail.com>
2016-02-21 22:29:03 -0430

    Create FlightGear.sh

---
+++ FlightGear.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#~ Instalador y desinstalador de Flightgear
+desinstala(){
+sudo pkcon remove flightgear fgrun
+rm -rv $HOME/.{fltk,fgfs}
+sudo find /usr -iname "*flightgear*" -type d -exec rm -rvi {} \;
+}
+
+instala() {
+cosa="pkcon"
+cosa="aptitude --visual-preview"
+
+sudo $cosa install flightgear fgrun
+}
+
+instala
