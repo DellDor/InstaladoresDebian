4550e81d87118bcadc6a83618dce3a7cd19c6ab6
Miguel Dell'Uomini <delldor@gmail.com>
2016-02-23 00:02:14 -0430

    Update FlightGear.sh

--- FlightGear.sh
+++ FlightGear.sh
@@ -3,12 +3,13 @@
 desinstala(){
 sudo pkcon remove flightgear fgrun
 rm -rv $HOME/.{fltk,fgfs}
+sdo rm -rv /root/.{fltk,fgfs}
 sudo find /usr -iname "*flightgear*" -type d -exec rm -rvi {} \;
 }
 
-instala() {
+instala(){
 cosa="pkcon"
-cosa="aptitude --visual-preview"
+#cosa="aptitude --visual-preview"
 
 sudo $cosa install flightgear fgrun
 sudo chmod a+rw -R $HOME/.fgfs
