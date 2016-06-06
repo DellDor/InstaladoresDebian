43350dc5c6276e86be4b8edf0f74218a30ad6de1
Miguel Dell'Uomini <delldor@gmail.com>
2016-06-06 11:59:39 -0430

    Actualizada versión a 4.3-i686

--- sparky-mate.sh
+++ sparky-mate.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 #~ chmod a+x /var/tmp/sparky-mate.sh
 #Inicia la creación de metapaquete de Sparky Mate.
+
+#~cd /tmp
+#~wget -c https://raw.githubusercontent.com/DellDor/InstaladoresDebian/master/sparky-mate.sh
+
+#6 junio 2016
 #31 enero 2016
 
 cd `mktemp -d`
 export servidor="http://sourceforge.net/projects/sparkylinux/files"
 export sector="mate" 
-export version="4.2-i586"
+export version="4.3-i686"
 export descargar="$servidor/$sector/sparkylinux-$version-$sector.iso.package-list.txt"
 
 wget -c https://raw.githubusercontent.com/DellDor/InstaladoresDebian/master/creameta-de-lista.sh
