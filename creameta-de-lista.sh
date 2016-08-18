63ccf5d3551cd0b0c29e09f34a3f49e0b961d3db
Miguel Dell'Uomini <delldor@gmail.com>
2016-08-17 21:33:45 -0400

    Add English comments, minor corrections

--- creameta-de-lista.sh
+++ creameta-de-lista.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
-#Crea metapaquete desde listados de paquetes publicados en la red.
-#Para ser llamado desde otro guión con la información de la fuente.
+#Crea metapaquete desde listados de paquetes publicados en la red, como recomendaciones y sugerencias. Pensado para Sparky linux (sourceforge.net) ser llamado desde otro guión con la información de la fuente.
+#Builds metapackages with packages listed on web (sourceforge.net) as recommends and suggests. Thinked to use with Sparkylinux and from another script. 
 
-#Usa las siguientes variables exportadas de otro guión:
-#export servidor="http://sourceforge.net/projects/sparkylinux/files"
-#export sector="mate" 
-#export version="4.2-i586"
-#export descargar="$servidor/$sector/sparkylinux-$version-$sector.iso.package-list.txt"
+#Variables:
+export servidor="http://sourceforge.net/projects/sparkylinux/files"
+export sector="lxqt" 
+export version="4.3-i686"
+export descargar="$servidor/$sector/sparkylinux-$version-$sector.iso.package-list.txt"
 
-#~ TODO:Corregir los errores que plantea Lintian
+#~ Hacer:Corregir los errores que plantea Lintian // Solve issues reported by lintian
 #~ E: delldor.sparky.mate-4.2-i586: debian-changelog-file-missing
 #~ E: delldor.sparky.mate-4.2-i586: no-copyright-file
 #~ W: delldor.sparky.mate-4.2-i586: essential-no-not-needed
 
-
-tempo=/var/tmp/sparky
-mkdir -p  $tempo
-
+#Crea directorio temporal de trabajo // Creates temporal working folder
+tempo=$(mktemp -d sparkyXXX)
 cd $tempo
-
+#Descarga lista // Download list
 wget -c $descargar
 
+#Añade a las depedencias todos los paquetes como recomendaciones, sin versión // Add packages as recommends without its versions.
+#Se puede usar como archivo lo que viene de otra máquina ejecutando dpkg -l
 archivo="sparkylinux-$version-$sector.iso.package-list.txt"
-
 recomienda=$(cat $archivo|grep ^ii |awk '{print $2}'|awk '{gsub(":i386","");print}'|awk '{printf "%s, ", $0}'| sed 's/,.$//')
 echo $recomienda
 
+#Añade a las depedencias todos los paquetes como sugerencias, con versiones // Add packages as suggests with its versions
 sugiere=$(cat $archivo|grep ^ii |awk '{print $2" (>="$3")"}'|awk '{gsub(":i386","");print}'|awk '{printf "%s, ", $0}'| sed 's/,.$//')
 echo $sugiere
 
@@ -34,22 +34,18 @@ fecha=`date +%F|tr '-' '.'`
 
 nombre="delldor.sparky.$sector-$version"
 
-descripcion="Metapaquete de paquetes desde red
+descripcion="Metapapackage from list in web
  Sparky $sector $version.
- Armado el $fecha.
- Lista de paquetes descargado desde
+ Builded $fecha.
+ Package list download from
  $servidor
  .
- Por Miguel Dell'Uomini
+ By Miguel Dell'Uomini
  .
  DellDor"
 
-#Versión del metapaquete
-#~ version=$fecha
 
-#directorio0=`mktemp -d`
-#~ directorio0=/var/tmp/sparky
-
+#Directorio para control y demás // Folder for control, etc
 directorio=$tempo/$nombre
 mkdir -p $directorio/DEBIAN
 
@@ -63,18 +59,18 @@ Version: $version
 Provides: $nombre
 Homepage: http://delldor.wordpress.com/
 Depends: gdebi (>=0.7)
-Recommends: $recomienda
+Recommends: aptitude, $recomienda
 Suggests: $sugiere
 Description: $descripcion" > $directorio/DEBIAN/control
 
 dpkg -b $directorio && {
-read -p "Listo el paquete. Pulse Enter para instalar instalará" a
+read -p "Listo el paquete. Pulse Enter para instalarlo. // Metapackage ready. Press Enter to install." a
 sudo gdebi  $tempo/$nombre.deb
 
 #Lo copia a la carpeta desde donde se llamó el guión
 #cp -v $directorio0/$nombre.deb `dirname "$1"`
 
-#~ TODO: Ejecutar solo si no está bloqueado el manejo de paquetes.
-read -p "Enter para llamar aptitude para revisar las recomendaciones" a
+#~ Hacer: Ejecutar solo si no está bloqueado el manejo de paquetes.
+read -p "Enter para llamar a aptitude para revisar las recomendaciones // Enter for execute aptitude for check recommends" a
 sudo aptitude -S $nombre
 }
