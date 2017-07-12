e6b7679d7a2140444959e29936f4b06515759eae
Miguel Dell'Uomini <delldor@gmail.com>
2017-07-12 12:42:03 -0400

    Create main.cmd

---
+++ aptitude-robot/main.cmd
@@ -0,0 +1,3 @@
+En esta carpeta irán los archivos para usar aptitude-robot para instalar en Debian Testing (sparky) los distintos tipos de elemenots.
+Cada archivo de estos va en:
+/etc/aptitude-robot/pkglist.d/
